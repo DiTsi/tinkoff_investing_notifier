677f0a0d7cae4f73f725bf7863157ab19fde2bed
Dmitry Tsybus <tsibous14@gmail.com>
2021-04-28 16:31:51 +0300

    telegram_bot, database, remove trash

--- tinkoff.py
+++ tinkoff.py
@@ -1,76 +1,39 @@
 import os
 import asyncio
-# import tinvest
-from tinvest import AsyncClient, SyncClient, InstrumentType
-
-
-def get_portfolio(TOKEN):
-    client = SyncClient(TOKEN)
-    portfolio = client.get_portfolio()
-    return portfolio
-
-
-def get_portfolio_currencies(TOKEN):
-    client = SyncClient(TOKEN)
-    portfolio_currencies = client.get_portfolio_currencies()
-    return portfolio_currencies
-
-
-def get_market_stocks(TOKEN):
-    client = SyncClient(TOKEN)
-    market_stocks = client.get_market_stocks()
-    return market_stocks.payload.instruments
-
-
-def get_stock_price(TOKEN, ticker):
-    stocks = get_market_stocks(TOKEN)
-    matches = [stock for stock in stocks if stock.ticker == ticker]
-    if len(matches) == 1:
-        return matches[0]
-
-
-def get_stocks(portfolio):
-    stocks = list()
-    payload = portfolio.payload
-    for position in payload.positions:
-        if position.instrument_type == InstrumentType.stock:
-            stocks.append(position)
-    return stocks
-
-
-def buy_price(stocks, tickers='ALL'):
-    if tickers == 'ALL':
-        tickers = ''
-        for stock in stocks:
-            tickers = tickers + stock.ticker + ','
-    for stock in stocks:
-        if stock.ticker in tickers.split(','):
-            print(f'Name: {stock.name}, Buy price: {stock.balance * stock.average_position_price.value}$')
-
-
-def currency_price(TOKEN, currency='USD'):
-    price = 0
-    currencies = get_portfolio_currencies(TOKEN)
-    if currency == 'USD':
-        for currency in currencies.payload.currencies:
-            price = price + currency.balance * currency.currency
+from time import sleep
+import tinvest
+from tinvest import AsyncClient, SyncClient, InstrumentType, CandleResolution, Streaming
+from database import init_db
+from sqlalchemy import create_engine
+from bot import daily_report
 
 
 def main():
     TOKEN = os.getenv('TOKEN')
-    portfolio = get_portfolio(TOKEN)
-    currencies = get_portfolio_currencies(TOKEN)
-    stocks = get_stocks(portfolio)
-    # buy_price(stocks, tickers='ENDP,ACAD')
-    e = get_stock_price(TOKEN, 'ACAD')
-    pass
+    MARIADB_HOST = os.getenv('MARIADB_HOST')
+    MARIADB_DB = os.getenv('MARIADB_DB')
+    MARIADB_USER = os.getenv('MARIADB_USER')
+    MARIADB_PASSWORD = os.getenv('MARIADB_PASSWORD')
+
+    client = SyncClient(TOKEN)
+
+    engine = create_engine(
+        f'mysql+pymysql://{MARIADB_USER}:{MARIADB_PASSWORD}@{MARIADB_HOST}/{MARIADB_DB}?charset=utf8mb4')
+    init_db(engine)
+
+    portfolio = client.get_portfolio()
+
+    my_figies = list()
+    for position in portfolio.payload.positions:
+        if position.instrument_type == InstrumentType.stock:
+            my_figies.append(position.figi)
+
+    for figi in my_figies:
+        a = client.get_market_orderbook(figi, 0)
+        timestamp_ = timestamp
+        last_price = client.get_market_orderbook(figi, 0).payload.last_price
 
 
-async def test():
-    TOKEN = os.getenv('TOKEN')
-    client = AsyncClient(TOKEN, use_sandbox=True)
-    await client.get_market_stocks()
-
-
-# asyncio.run(test())
 main()
+
+# daily_report()
